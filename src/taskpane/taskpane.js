1992d1a6e72d52f20133efcd4bf0aafa41b16167
Matan P <mapaz@microsoft.com>
2020-01-18 21:10:50 +0200

    cell-change handler poc

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,8 +10,51 @@ Office.onReady(info => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+
+    logic();
+    
   }
 });
+
+function logic() {
+  // Create myTable
+  Excel.run(function (ctx) {
+      return ctx.sync().then(function () {
+          //Create a new binding for A1
+          Office.context.document.bindings.addFromNamedItemAsync("Sheet1!A1", Office.BindingType.Text, { id: "A1" }, function (asyncResult) {
+              if (asyncResult.status == "failed") {
+                  console.log("Action failed with error: " + asyncResult.error.message);
+              }
+              else {
+                  // If successful, add the event handler to the table binding.
+                  Office.select("bindings#A1").addHandlerAsync(Office.EventType.BindingDataChanged, onBindingDataChanged);
+              }
+          });
+      })
+      .catch(function (error) {
+          console.log(JSON.stringify(error));
+      });
+  });
+};
+  
+  // When data in A1 is changed, this event is triggered.
+  function onBindingDataChanged(_eventArgs) {
+    Excel.run(function (ctx) {
+        // Highlight the table in orange to indicate data changed.
+        var sheet = ctx.workbook.worksheets.getItem("Sheet1");
+        var cell = sheet.getRange("A1");
+        var fill = cell.format.fill;
+        fill.load("color");
+        return ctx.sync().then(function () {
+          if (fill.color != "Orange") {
+            cell.format.fill.color = "Orange";
+            console.log("The value in this table got changed!");
+          }
+        }).then(ctx.sync).catch(function (error) {
+            console.log(JSON.stringify(error));
+        });
+    });
+  } 
 
 export async function run() {
   try {
@@ -28,7 +71,7 @@ export async function run() {
       range.format.fill.color = "yellow";
 
       await context.sync();
-      console.log(`The range address was ${range.address}.`);
+      console.warn(`The range address was ${range.address}.`);
     });
   } catch (error) {
     console.error(error);
